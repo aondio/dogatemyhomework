4d3745a4c4f40d35b7547a92240db63d7fa2c26f
Arianna Aondio <arianna.aondio@varnish-software.com>
2016-03-28 13:15:52 +0200

    Stupid excuse random generated

--- src/vmod_dogatemyhomework.c
+++ src/vmod_dogatemyhomework.c
@@ -6,6 +6,13 @@
 
 #include "vcc_if.h"
 
+static const char *crap[] = {
+		"I’d just put a casserole in the oven",
+		"My plastic surgery (for enhancement purposes) needed some \"tweaking\" to get it just right",
+		"I was sitting in the bathroom and my feet and legs fell asleep. When I stood, up I fell and broke my ankle ",
+};
+
+
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
@@ -13,14 +20,20 @@ init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 }
 
 VCL_STRING
-vmod_hello(const struct vrt_ctx *ctx, VCL_STRING name)
+vmod_dogatemyhomework(const struct vrt_ctx *ctx)
 {
+	int len;
+	len = (sizeof(crap)/ sizeof(crap[0])) - 1;
+
 	char *p;
 	unsigned u, v;
+	int i;
 
 	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
 	p = ctx->ws->f;		/* Front of workspace area */
-	v = snprintf(p, u, "Hello, %s", name);
+
+	i = rand()%len;
+	v = snprintf(p, u, "I can't come to work because %s", crap[i]);
 	v++;
 	if (v > u) {
 		/* No space, reset and leave */
