4203ce2ba413a37d1f9dbc41b3572e66a40ee6f5
vagrant <vagrant@ubuntu-14.04-amd64.local>
2016-04-19 13:28:09 +0000

    Make the vmod ready for the summit

--- src/vmod_dogatemyhomework.c
+++ src/vmod_dogatemyhomework.c
@@ -6,41 +6,39 @@
 
 #include "vcc_if.h"
 
-static const char *crap[] = {
-		"I’d just put a casserole in the oven",
-		"My plastic surgery (for enhancement purposes) needed some \"tweaking\" to get it just right",
-		"I was sitting in the bathroom and my feet and legs fell asleep. When I stood, up I fell and broke my ankle ",
+int i;
+const char *crap[] = {
+	"Constipation has made me a walking time bomb.",
+	"I super-glued my eye thinking it was contact solution.",
+	"My left turn signal was out so I had to make all right turns to get to work.",
+	"I made the mistake of letting in some Jehovah's Witnesses and they wouldn't leave.",
+	"I was sitting in the bathroom and my feet and legs fell asleep. When I stood, up I fell and broke my ankle",
+	"I won't be coming in to work today. My wife informed me that she is going to conceive today, and I really want to be there when it happens.",
+	NULL
 };
-
 
 int
 init_function(struct vmod_priv *priv, const struct VCL_conf *conf)
 {
+	i = rand()%6;
 	return (0);
 }
 
 VCL_STRING
 vmod_dogatemyhomework(const struct vrt_ctx *ctx)
 {
-	int len;
-	len = (sizeof(crap)/ sizeof(crap[0])) - 1;
 
 	char *p;
 	unsigned u, v;
-	int i;
 
-	u = WS_Reserve(ctx->ws, 0); /* Reserve some work space */
-	p = ctx->ws->f;		/* Front of workspace area */
-
-	i = rand()%len;
+	u = WS_Reserve(ctx->ws, 0);
+	p = ctx->ws->f;
 	v = snprintf(p, u, "I can't come to work because %s", crap[i]);
 	v++;
 	if (v > u) {
-		/* No space, reset and leave */
 		WS_Release(ctx->ws, 0);
 		return (NULL);
 	}
-	/* Update work space with what we've used */
 	WS_Release(ctx->ws, v);
 	return (p);
 }
